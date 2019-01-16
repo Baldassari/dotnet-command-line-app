1feef8c9e8e6f71e0cc379ab50bac88ecd0374fd
asaph.souza <asaph.souza@novabase.pt>
2019-01-16 19:09:31 +0000

    Async Tasks

--- Program.cs
+++ Program.cs
@@ -9,19 +9,8 @@ namespace TeleprompterConsole
 	{
 		static void Main(string[] args)
 		{
-			var lines = ReadFrom("sampleQuotes.txt");
-			foreach (var line in lines)
-			{
-				Console.Write(line);
-				if (!string.IsNullOrWhiteSpace(line))
-				{
-					var pause = Task.Delay(200);
-					// Synchronously waiting on a task is an
-					// anti-pattern. This will get fixed in later
-					// steps.
-					pause.Wait();
-				}
-			}
+
+			ShowTeleprompter().Wait();
 		}
 		static IEnumerable<string> ReadFrom(string file)
 		{
@@ -31,13 +20,35 @@ namespace TeleprompterConsole
 				while ((line = reader.ReadLine()) != null)
 				{
 					var words = line.Split(' ');
+					var lineLength = 0;
 					foreach (var word in words)
 					{
 						yield return word + " ";
+						lineLength += word.Length + 1;
+						if (lineLength > 70)
+						{
+							yield return Environment.NewLine;
+							lineLength = 0;
+						}
 					}
 					yield return Environment.NewLine;
 				}
 			}
 		}
+		private static async Task ShowTeleprompter()
+		{
+
+			var words = ReadFrom("sampleQuotes.txt");
+
+			foreach (var word in words)
+			{
+				Console.Write(word);
+				if (!string.IsNullOrWhiteSpace(word))
+				{
+					await Task.Delay(200);
+				}
+			}
+		}
+
 	}
 }
